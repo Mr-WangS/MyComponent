8caa48c7e00e328fbed47736329b1e9c1df60942
Mr-WangS <wang793717096@gamil.com>
2018-08-16 16:28:25 +0800

    添加项目

---
+++ pages/tab/tab.js
@@ -0,0 +1,37 @@
+Page({
+  data: {
+    tabIndex:0 ,
+    transPer:0,
+  },
+  onLoad: function () {
+    this.setData({
+      tabNav: {
+        tabnum: 3,
+        tabitem: [
+          {
+            "text": "Tab1"
+          },
+          {
+            "text": "Tab2"
+          },
+          {
+            "text": "Tab3"
+          }
+        ]
+      },
+    })
+  },
+  tabClick(e){
+
+    this.setData({
+      tabIndex: e.currentTarget.dataset.tabid,
+      
+       })
+    this.setData({
+      transPer: this.data.tabIndex * 100 / this.data.tabNav.tabnum
+    })
+ 
+  },
+
+
+})
