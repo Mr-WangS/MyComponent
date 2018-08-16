681cfaed0e8c9274a8b46a02752b88ff14302d4a
Mr-WangS <wang793717096@gamil.com>
2018-08-16 17:21:27 +0800

    import

---
+++ pages/import/import.js
@@ -0,0 +1,70 @@
+// pages/import/import.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    throwData:" I am man,and you like woman!"
+  },
+  retFs(){
+    // let ssr = this.data.throwData.replace(/,/g,'')
+    // this.setData({ throwData: this.data.throwData.split(",").reverse()})
+    console.log('事件传递')
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
